0257c0e31e409302a53b8d85c9224d40cf1b6d90
hrishi123-cpu <64744997+hrishi123-cpu@users.noreply.github.com>
2021-05-12 20:15:19 +0530

    Functional Component - Greet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React, {Component} from 'react';
+import Greet from './components/Greet'
 
 class App extends Component{
     render(){
   return (
     <div className="App">
       <h1>Hello this is my first app </h1>
+      <Greet/>
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1> Hello </h1>
+// }
+const Greet = () => <h1>Hello Hrishi</h1>
+
+export default Greet;
