6e780a9c9d902b58ba2bd72c4f1acd69321bc224
hrishi123-cpu <64744997+hrishi123-cpu@users.noreply.github.com>
2021-05-12 20:37:29 +0530

    JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-// const Hello =() =>{
-//     return(
-//         <div className="dummyClass">
-//             <h1>hello</h1>  // using jsx
-//         </div>
-//     )
-// }
 const Hello =() =>{
-    return React.createElement(
-        'div',
-        {id:'hello', className: 'dummyClass'},    //attribute
-        React.createElement('h1',null,'hello'))     //Without JSX
+    return(
+        <div className="dummyClass">
+            <h1>hello</h1> 
+        </div>
+    )
 }
+// const Hello =() =>{
+//     return React.createElement(
+//         'div',
+//         {id:'hello', className: 'dummyClass'},    //attribute
+//         React.createElement('h1',null,'hello'))     //Without JSX
+// }
 export  default Hello; 
