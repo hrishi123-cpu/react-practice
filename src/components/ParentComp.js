51a09cddcb17452555594e5aad20dc526ebf7b14
hrishi123-cpu <64744997+hrishi123-cpu@users.noreply.github.com>
2021-05-16 11:45:46 +0530

    memo component
    Similar as pure comp but memo is for functional component

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MemoComp from './MemoComp'
 import PureComp from './PureComp'
 import RegComp from './RegComp'
 
@@ -22,8 +23,9 @@ import RegComp from './RegComp'
         return (
             <div>
                 Parent component
-                <RegComp name={this.state.name}></RegComp>
-                <PureComp name={this.state.name}></PureComp>
+                {/* <RegComp name={this.state.name}></RegComp>
+                <PureComp name={this.state.name}></PureComp> */}
+                <MemoComp name={this.state.name}></MemoComp>
             </div>
         )
     }
